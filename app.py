f07bc7eba1b34d3da7781f613da70a72c8b73e6e
Sharon Kuninin <schnitzelftw@gmail.com>
2022-08-29 14:04:44 +0300

    Add an abstract solution class

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
+from typing import List, Tuple
+
+from entities.contributor import Contributor
+from entities.project import Project
+
+
+def parse(lines: List[str]) -> Tuple[List[Contributor], List[Project]]:
+    pass
+
+
 if __name__ == '__main__':
     pass
