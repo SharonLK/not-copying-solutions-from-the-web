0b113f91a9d36a0375ff2472654782768df36c96
Sharon Kuninin <schnitzelftw@gmail.com>
2022-08-29 14:10:56 +0300

    Begin adding parsing capabilities

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ from entities.project import Project
 
 
 def parse(lines: List[str]) -> Tuple[List[Contributor], List[Project]]:
-    pass
+    contributors: List[Contributor] = []
+    projects: List[Project] = []
+
+    num_contributors, num_projects = (int(s) for s in lines[0].split())
+
+    print(num_contributors, num_projects)
+
+    return contributors, projects
 
 
 if __name__ == '__main__':
-    pass
+    with open('data_sets/a_an_example.in.txt', 'r') as f:
+        parse(f.readlines())
